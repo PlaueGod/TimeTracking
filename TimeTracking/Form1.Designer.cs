1da0fd2873e456e9e91a6d6b06abae63f188a02c
Ivan <redkiibarli@gmail.com>
2025-02-05 12:40:08 +0200

    Add project files.

---
+++ TimeTracking/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace TimeTracking
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            openFileExe = new OpenFileDialog();
+            textBox1 = new TextBox();
+            ClearTextBox = new Button();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            textBox2 = new TextBox();
+            PathOE = new Label();
+            WriteT = new Label();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            GoBlock = new Button();
+            exitLabel = new Label();
+            label1 = new Label();
+            hideBut = new Label();
+            SuspendLayout();
+            // 
+            // openFileExe
+            // 
+            openFileExe.FileName = "openFileExe";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Cursor = Cursors.Hand;
+            textBox1.Font = new Font("Microsoft JhengHei", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(51, 65);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(627, 36);
+            textBox1.TabIndex = 0;
+            textBox1.Click += textBox1_Click_1;
+            textBox1.TextChanged += textBox1_Click;
+            // 
+            // ClearTextBox
+            // 
+            ClearTextBox.Cursor = Cursors.Hand;
+            ClearTextBox.FlatStyle = FlatStyle.Popup;
+            ClearTextBox.Font = new Font("Sitka Heading Semibold", 12.249999F, FontStyle.Bold);
+            ClearTextBox.Location = new Point(603, 117);
+            ClearTextBox.Name = "ClearTextBox";
+            ClearTextBox.Size = new Size(75, 45);
+            ClearTextBox.TabIndex = 1;
+            ClearTextBox.Text = "Clear";
+            ClearTextBox.UseVisualStyleBackColor = true;
+            ClearTextBox.Click += ClearTextBox_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaptionText;
+            panel1.Location = new Point(0, 487);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(735, 95);
+            panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ActiveCaptionText;
+            panel2.Location = new Point(0, -6);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(735, 26);
+            panel2.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Font = new Font("Microsoft JhengHei", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(539, 222);
+            textBox2.MaxLength = 3;
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(139, 35);
+            textBox2.TabIndex = 4;
+            textBox2.KeyPress += textBox2_KeyPress;
+            // 
+            // PathOE
+            // 
+            PathOE.AutoSize = true;
+            PathOE.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            PathOE.Location = new Point(503, 32);
+            PathOE.Name = "PathOE";
+            PathOE.Size = new Size(139, 19);
+            PathOE.TabIndex = 5;
+            PathOE.Text = "Path of exe file pls";
+            // 
+            // WriteT
+            // 
+            WriteT.AutoSize = true;
+            WriteT.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            WriteT.Location = new Point(241, 179);
+            WriteT.Name = "WriteT";
+            WriteT.Size = new Size(401, 19);
+            WriteT.TabIndex = 6;
+            WriteT.Text = "Write the time you want to spend in the app(In minuts)";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Tahoma", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(223, 278);
+            label3.Name = "label3";
+            label3.Size = new Size(419, 19);
+            label3.TabIndex = 8;
+            label3.Text = "Write the time for which you want to block the application";
+            // 
+            // textBox3
+            // 
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Font = new Font("Microsoft JhengHei", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(539, 327);
+            textBox3.MaxLength = 3;
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(139, 35);
+            textBox3.TabIndex = 7;
+            textBox3.KeyPress += textBox3_KeyPress;
+            // 
+            // GoBlock
+            // 
+            GoBlock.Cursor = Cursors.Hand;
+            GoBlock.FlatStyle = FlatStyle.Popup;
+            GoBlock.Font = new Font("Sitka Heading Semibold", 12.249999F, FontStyle.Bold);
+            GoBlock.Location = new Point(603, 399);
+            GoBlock.Name = "GoBlock";
+            GoBlock.Size = new Size(75, 45);
+            GoBlock.TabIndex = 9;
+            GoBlock.Text = "Block";
+            GoBlock.UseVisualStyleBackColor = true;
+            GoBlock.Click += GoBlock_Click;
+            // 
+            // exitLabel
+            // 
+            exitLabel.AutoSize = true;
+            exitLabel.BorderStyle = BorderStyle.Fixed3D;
+            exitLabel.Cursor = Cursors.Hand;
+            exitLabel.FlatStyle = FlatStyle.Flat;
+            exitLabel.Font = new Font("Segoe UI", 15F);
+            exitLabel.Location = new Point(697, 25);
+            exitLabel.Name = "exitLabel";
+            exitLabel.Size = new Size(26, 30);
+            exitLabel.TabIndex = 11;
+            exitLabel.Text = "X";
+            exitLabel.Click += exitLabel_Click;
+            exitLabel.MouseEnter += exitLabel_MouseEnter;
+            exitLabel.MouseLeave += exitLabel_MouseLeave;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(51, 134);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 15);
+            label1.TabIndex = 12;
+            label1.Text = "Time:";
+            // 
+            // hideBut
+            // 
+            hideBut.AutoSize = true;
+            hideBut.BorderStyle = BorderStyle.Fixed3D;
+            hideBut.Cursor = Cursors.Hand;
+            hideBut.FlatStyle = FlatStyle.Flat;
+            hideBut.Font = new Font("Segoe UI", 15F);
+            hideBut.Location = new Point(665, 25);
+            hideBut.Name = "hideBut";
+            hideBut.Size = new Size(22, 30);
+            hideBut.TabIndex = 13;
+            hideBut.Text = "-";
+            hideBut.Click += hideBut_Click;
+            hideBut.MouseEnter += hideBut_MouseEnter;
+            hideBut.MouseLeave += hideBut_MouseLeave;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(727, 508);
+            Controls.Add(hideBut);
+            Controls.Add(label1);
+            Controls.Add(exitLabel);
+            Controls.Add(GoBlock);
+            Controls.Add(label3);
+            Controls.Add(textBox3);
+            Controls.Add(WriteT);
+            Controls.Add(PathOE);
+            Controls.Add(textBox2);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(ClearTextBox);
+            Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            Resize += Form1_Resize;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private OpenFileDialog openFileExe;
+        private TextBox textBox1;
+        private Button ClearTextBox;
+        private Panel panel1;
+        private Panel panel2;
+        private TextBox textBox2;
+        private Label PathOE;
+        private Label WriteT;
+        private Label label3;
+        private TextBox textBox3;
+        private Button GoBlock;
+        private Label exitLabel;
+        private Label label1;
+        private Label hideBut;
+    }
+}
---
+++ TimeTracking/Form1.cs
@@ -0,0 +1,252 @@
+using System.Diagnostics;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace TimeTracking
+{
+    public partial class Form1 : Form
+    {
+        private string appPath;
+        private CancellationTokenSource cts;
+        private double remainingUsageTime;
+        private DateTime blockEndTime;
+        private bool isTimerRunning = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EnableFormDragging(this);
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void textBox1_Click_1(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Executable Files|*.exe";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    appPath = openFileDialog.FileName;
+                    textBox1.Text = appPath;
+                }
+            }
+        }
+
+        private void ClearTextBox_Click(object sender, EventArgs e)
+        {
+            cts?.Cancel();
+            isTimerRunning = false;
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            label1.Text = "Time:";
+            MessageBox.Show("Timer Clear.");
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private async void GoBlock_Click(object sender, EventArgs e)
+        {
+            if (isTimerRunning)
+                return;
+
+            if (string.IsNullOrEmpty(appPath) || !File.Exists(appPath))
+            {
+                MessageBox.Show("Select the correct exe file.");
+                return;
+            }
+
+            if (!double.TryParse(textBox2.Text, out double usageTime) || usageTime <= 0 ||
+                !double.TryParse(textBox3.Text, out double blockTime) || blockTime <= 0)
+            {
+                MessageBox.Show("Enter the correct time in minutes.");
+                return;
+            }
+
+            cts = new CancellationTokenSource();
+            remainingUsageTime = usageTime;
+            isTimerRunning = true;
+            UpdateTimerDisplay();
+
+            while (remainingUsageTime > 0)
+            {
+                if (cts.Token.IsCancellationRequested)
+                {
+                    isTimerRunning = false;
+                    return;
+                }
+
+                if (IsAppRunning())
+                {
+                    remainingUsageTime -= 1.0 / 60; // ”меньшаем каждую секунду
+                }
+
+                UpdateTimerDisplay();
+                await Task.Delay(1000); // ∆дем секунду
+            }
+
+            MessageBox.Show("The usage time is over. Block the application.", "Warning",
+                MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+            blockEndTime = DateTime.Now.AddMinutes(blockTime);
+            await BlockApp(blockTime, cts.Token);
+            isTimerRunning = false;
+        }
+        private async Task BlockApp(double blockTime, CancellationToken token)
+        {
+            while (DateTime.Now < blockEndTime)
+            {
+                if (token.IsCancellationRequested)
+                    return;
+
+                var processes = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(appPath));
+                foreach (var process in processes)
+                {
+                    try
+                    {
+                        process.Kill();
+                    }
+                    catch { }
+                }
+                UpdateTimerDisplay();
+                await Task.Delay(1000);
+            }
+
+            MessageBox.Show("The blocking time has expired. You can start the application again.");
+        }
+
+        private bool IsAppRunning()
+        {
+            return Process.GetProcessesByName(Path.GetFileNameWithoutExtension(appPath)).Any();
+        }
+
+        private void UpdateTimerDisplay()
+        {
+            if (remainingUsageTime > 0)
+            {
+                label1.Text = $"Time left to use the app: {remainingUsageTime:F1} min";
+            }
+            else if (DateTime.Now < blockEndTime)
+            {
+                label1.Text = $"App lock time: {(blockEndTime - DateTime.Now).TotalMinutes:F1} min";
+
+                if (blockEndTime < DateTime.Now)
+                {
+                    label1.Text = "You can run the application";
+                }
+
+            }
+            else
+            {
+            }
+        }
+
+
+
+        private void exitLabel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void exitLabel_MouseEnter(object sender, EventArgs e)
+        {
+            exitLabel.BackColor = Color.PaleVioletRed;
+        }
+
+        private void exitLabel_MouseLeave(object sender, EventArgs e)
+        {
+            exitLabel.BackColor = SystemColors.Control;
+        }
+        private static Point _lastPoint;
+        public static void EnableFormDragging(Form form)
+        {
+            form.MouseDown += (s, e) =>
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    _lastPoint = e.Location;
+                }
+            };
+
+            form.MouseMove += (s, e) =>
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    form.Left += e.X - _lastPoint.X;
+                    form.Top += e.Y - _lastPoint.Y;
+                }
+            };
+        }
+
+        private async void hideBut_Click(object sender, EventArgs e)
+        {
+            for (double i = 1.0; i >= 0; i -= 0.05)
+            {
+                this.Opacity = i;
+                await Task.Delay(10);
+            }
+            this.WindowState = FormWindowState.Minimized;
+            this.Opacity = 1;
+
+        }
+
+        private void hideBut_MouseEnter(object sender, EventArgs e)
+        {
+            hideBut.BackColor = Color.SkyBlue;
+        }
+
+        private void hideBut_MouseLeave(object sender, EventArgs e)
+        {
+            hideBut.BackColor = SystemColors.Control;
+        }
+
+        private async void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+                return;
+
+            this.Opacity = 0; 
+            this.Show();
+
+            for (double i = 0; i <= 1; i += 0.05) 
+            {
+                this.Opacity = i;
+                await Task.Delay(10);
+            }
+        }
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_SHOWWINDOW = 0x18;
+
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_SHOWWINDOW && m.WParam.ToInt32() == 0)
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+    }
+}
